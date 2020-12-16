83eceb86266ad373cfcadf63ec9f26b029be88c7
Jena Boehm <boehmjena@gmail.com>
2020-12-15 22:24:57 -0800

    passing get all logs test

--- __tests__/logs.test.js
+++ __tests__/logs.test.js
@@ -39,4 +39,45 @@ describe('log routes', () => {
         expect(res.body).toEqual({ ...log, id: '2' });
       });
   });
+
+  it('returns all logs', async() => {
+    const recipe = await Recipe.insert({
+      name: 'cookies',
+      directions: [
+        'preheat oven to 375',
+        'mix ingredients',
+        'put dough on cookie sheet',
+        'bake for 10 minutes'
+      ]
+    });
+
+    const logs = await Promise.all([
+      {
+        dateOfEvent: '12/24/20',
+        notes: 'here are some notes',
+        rating: '10/10',
+        recipeId: recipe.id
+      },
+      {
+        dateOfEvent: '12/24/20',
+        notes: 'here are some notes',
+        rating: '10/10',
+        recipeId: recipe.id
+      },
+      {
+        dateOfEvent: '12/24/20',
+        notes: 'here are some notes',
+        rating: '10/10',
+        recipeId: recipe.id
+      }
+    ].map(log => Log.insert(log)));
+
+    return request(app)
+      .get('/api/v1/logs')
+      .then(res => {
+        logs.forEach(log => {
+          expect(res.body).toContainEqual(log);
+        });
+      });
+  });
 });
